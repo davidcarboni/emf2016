597a6e682692cd9a2dad8fb98d6cc7798caeef27
David Carboni <david@carboni.io>
2016-08-05 16:41:35 +0100

    Route placeholders now working.

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, jsonify
-from jwt import encode, decode
 
 
 app = Flask(__name__)
@@ -10,13 +9,8 @@ def hello():
     return "Hello, this is the EMF 2016 number '1' sign! To get started, try a GET to <a href='/lights'>/info</a> or <a href='/lights'>/lights</a>."
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
-
 @app.route('/info', methods=['GET'])
-def demo():
+def info():
     return """This API lets you control the lighs in the nember '1' on the hill above the camp.<br/>
     By default it's a binary clock. The top five lights represent the hour and the bottom three lights are the number of 10-minutes into the hour.<br/>
     You can get make the lights flash by GETting <a href='/lights'>/lights</a> (e.g. in your browser)<br/>"""
@@ -28,5 +22,10 @@ def demo():
 
 
 @app.route('/lights', methods=['POST'])
-def demo():
+def custom():
     return "Soon this will let you post a custom lighting sequence.."
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
