aa246578b2026292d7e0f2c45c3499c47234bcb7
David Carboni <david@carboni.io>
2016-08-05 17:32:30 +0100

    Updated default port to 80

--- api.py
+++ api.py
@@ -118,5 +118,5 @@ class clock_thread(threading.Thread):
 if __name__ == '__main__':
     setup()
     start_clock()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(debug=True, host='0.0.0.0', port=port)
