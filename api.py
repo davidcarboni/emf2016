30491b58ce6ff64d0c09b7353ab95f42af1cd86a
David Carboni <david@carboni.io>
2016-08-05 17:14:31 +0100

    Brought in clock functionality.

--- api.py
+++ api.py
@@ -1,16 +1,18 @@
 import os
 from flask import Flask, request, jsonify
+import sys
+import threading
+import math
+from time import sleep
+from os import system
+from datetime import datetime
 import RPi.GPIO as GPIO
+
+THREADS = []
+chan_list = []
 
 
 app = Flask(__name__)
-
-
-def setup():
-	global chan_list
-	chan_list = [11,12,13,15,16,18,22,7]
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(chan_list, GPIO.OUT)
 
 
 @app.route("/", methods=['GET'])
@@ -35,7 +37,86 @@ def custom():
     return "Soon this will let you post a custom lighting sequence.."
 
 
+def setup():
+	global chan_list
+	chan_list = [11,12,13,15,16,18,22,7]
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(chan_list, GPIO.OUT)
+
+def start_clock():
+	global THREADS
+	clock = clock_thread()
+	clock.start()
+	THREADS.append(clock)
+
+
+def updatepins(timecode):
+    off_list = []
+    on_list = []
+    print(timecode)
+    for x in range(0, 8):
+        if timecode % 2 == 0:
+            off_list.append(chan_list[7 - x])
+        else:
+            on_list.append(chan_list[7 - x])
+        timecode = timecode >> 1
+    GPIO.output(on_list, False)
+    GPIO.output(off_list, True)
+
+
+def playhour(timecode):
+    GPIO.output(chan_list, False)
+    sleep(0.5)
+    GPIO.output(chan_list, True)
+    sleep(0.5)
+    GPIO.output(chan_list, False)
+    sleep(0.3)
+    for x in range(0, 8):
+        sleep(0.2)
+        GPIO.output(chan_list[x], True)
+    updatepins(timecode)
+
+
+def playquarter(timecode):
+    for y in range(0, 5):
+        for x in range(1, 8):
+            sleep(0.2)
+            GPIO.output(chan_list[x - 1], True)
+            GPIO.output(chan_list[x], False)
+        for x in range(7, 0, -1):
+            sleep(0.2)
+            GPIO.output(chan_list[x], True)
+            GPIO.output(chan_list[x - 1], False)
+    updatepins(timecode)
+
+class clock_thread(threading.Thread):
+	def __init__(self):
+		self.alive = True
+		threading.Thread.__init__(self)
+
+
+	def run(self):
+		while self.alive:
+			ctime = datetime.now()
+			hour = ctime.hour
+
+			tenmins = int(math.floor(ctime.minute / 10))
+
+			timecode = (hour << 3) + tenmins
+
+			if ctime.minute == 0:
+				playhour(timecode)
+			elif ctime.minute % 10 == 0:
+				playquarter(timecode)
+			else:
+				updatepins(timecode)
+
+			sleep(60)
+			pass
+
+
 if __name__ == '__main__':
     setup()
+    start_clock()
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
