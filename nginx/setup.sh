69e0d07d5f1b7dcb0cf68df5c71610187fd86265
David Carboni <david@carboni.io>
2016-08-07 20:33:14 +0100

    Added some sudos.

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -2,9 +2,9 @@
 
 directory=$PWD
 sudo apt-get update && sudo apt-get -y install nginx
-cp lights /etc/nginx/sites-available/
+sudo cp lights /etc/nginx/sites-available/
 cd /etc/nginx/sites-enabled
-ln -s ../sites-available/lights
-rm /etc/nginx/sites-enabled/default
+sudo ln -s ../sites-available/lights
+sudo rm /etc/nginx/sites-enabled/default
 
 cd $directory
